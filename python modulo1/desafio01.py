ead4638fa1db22255c0fb1e122273f82829c3a49
davi_barros <140422542+XDrake-656@users.noreply.github.com>
2026-09-11 19:07:00 -0300

    Add files via upload

---
+++ python modulo1/desafio01.py
@@ -0,0 +1,2 @@
+nome=input("olá, Qual é o seu nome? ")
+print("olá, entao seu nome é",nome+", prazer em te conhecer",nome+".")
---
+++ python modulo1/desafio03.py
@@ -0,0 +1,4 @@
+n1=input("Digite um número: ")
+n2=input("Digite outro número: ")
+soma=(int(n1)+int(n2))
+print("A soma entre",n1,"e",n2,"é igual a",soma)
